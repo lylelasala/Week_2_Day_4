696ef436e47dc2ff3b6eee0a82796d861edcb8c9
lylelasala <102710270+lylelasala@users.noreply.github.com>
2023-04-03 23:26:35 -0700

    Add files via upload

---
+++ day_4/module.py
@@ -0,0 +1,8 @@
+def square_footage(length, width):
+    """Calculate the square footage of a house."""
+    return length * width
+
+
+def circle_circumference(radius):
+    """Calculate the circumference of a circle."""
+    return 2 * 3.14159 * radius
